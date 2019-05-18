8d13f0ba3fa70588ce00353e5f436234889d48ea
Chris Norman <chris.norman2@googlemail.com>
2019-05-18 16:50:10 +0100

    Made it work (finally).

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 import os
 
-from functools import partial
-
 import wx
+from wx.lib.intctrl import IntCtrl
 
 from attr import attrs, attrib
 from keyboard import send
@@ -17,6 +16,7 @@ from speech import speak
 app = wx.App()
 filename = os.path.join(wx.GetHomeDir(), 'oht.yaml')
 frame = SizedFrame(None, title='Onehanded Typing')
+timer = wx.Timer(frame)
 panel = frame.GetContentsPane()
 panel.SetSizerType('form')
 
@@ -24,7 +24,6 @@ panel.SetSizerType('form')
 class state:
     """Application state."""
 
-    id = -1  # The id of the most recently created hotkey.
     last_name = None  # The last hotkey that was pressed.
     last_alternative = None
     alternative_index = -1
@@ -51,6 +50,7 @@ def bind(control, event_type):
 hotkey_names = []
 hotkey_ids = {}
 hotkey_alternatives = {}
+hotkey_convertions = {}
 
 hotkeys_label = wx.StaticText(panel, label='&Hotkeys')
 hotkeys = wx.ListCtrl(panel, style=wx.LC_REPORT)
@@ -61,42 +61,78 @@ alternatives = wx.ListCtrl(panel, style=wx.LC_REPORT)
 alternatives.AppendColumn('Friendly Name')
 alternatives.AppendColumn('Keys')
 
-add_alternative = wx.Button(panel, label='&New Key')
-remove_alternative = wx.Button(panel, label='&Delete Key')
+add_alternative_button = wx.Button(panel, label='&New Key')
+remove_alternative_button = wx.Button(panel, label='&Delete Key')
 
-register = wx.Button(panel, label='&Register Hotkey')
-unregister = wx.Button(panel, label='&Unregister Hotkey')
+register_hotkey_button = wx.Button(panel, label='&Register Hotkey')
+unregister_hotkey_button = wx.Button(panel, label='&Unregister Hotkey')
+bypass = wx.CheckBox(panel, label='&Bypass')
+interval = IntCtrl(panel, value=500, min=250, max=5000)
 
 
-def press_alternative(alternative):
+@bind(frame, wx.EVT_TIMER)
+def on_timer(event):
+    """The time is up."""
+    press_alternative()
+
+
+@bind(bypass, wx.EVT_CHECKBOX)
+def on_bypass(event):
+    """Temporarily unbind all hotkeys."""
+    if bypass.GetValue():
+        # Let's unbind all hotkeys.
+        unbind_hotkeys()
+    else:
+        # Re-bind hotkeys.
+        bind_hotkeys()
+
+
+def unbind_hotkeys():
+    """Unbind all hotkeys."""
+    for id in hotkey_ids.values():
+        frame.UnregisterHotKey(id)
+
+
+def bind_hotkeys():
+    """Bind all hotkeys."""
+    for name, id in hotkey_ids.items():
+        frame.RegisterHotKey(id, 0, vk_codes[name])
+
+
+def press_alternative():
     """Press some keys if state hasn't changed."""
-    if state.last_alternative is alternative:
-        keys = alternative.keys
-        send(keys)
-        state.last_name = None
-        state.alternative_index = -1
-        state.last_alternative = None
+    unbind_hotkeys()
+    keys = state.last_alternative.keys
+    send(keys)
+    state.last_name = None
+    state.alternative_index = -1
+    state.last_alternative = None
+    bind_hotkeys()
 
 
-def on_hotkey(name, event):
+@bind(frame, wx.EVT_HOTKEY)
+def on_hotkey(event):
     """A hotkey has been pressed."""
-    alternatives = hotkey_alternatives[name]
-    state.alternative_index += 1
+    name = hotkey_convertions[event.GetRawKeyCode()]
+    alternatives = hotkey_alternatives.get(name, None)
+    if alternatives is None:
+        return wx.Bell()
     if state.last_name == name:
+        # Move through the list.
+        state.alternative_index += 1
         if state.alternative_index >= len(alternatives):
             state.alternative_index = 0
     else:
         if state.last_name is not None:
-            press_alternative(state.last_alternative)
+            # The user is switching keys.
+            timer.Stop()
+            press_alternative()
         state.last_name = name
         state.alternative_index = 0  # Go back to the start.
-    try:
-        alternative = alternatives[state.alternative_index]
-    except IndexError:
-        return wx.Bell()
-    speak(alternative.name)
+    alternative = alternatives[state.alternative_index]
     state.last_alternative = alternative
-    wx.CallLater(0.25, press_alternative, alternative)
+    speak(alternative.name)
+    timer.Start(interval.GetValue(), oneShot=True)
 
 
 def message(message, caption):
@@ -115,23 +151,29 @@ def get_key():
             return keys[index]
 
 
-def register_hotkey(name, keys):
+def register_hotkey(name):
     """Given a name and a list of entries, registers a new hotkey."""
     value = vk_codes[name]
-    frame.Bind(wx.EVT_HOTKEY, partial(on_hotkey, name), id=state.id)
-    res = frame.RegisterHotKey(state.id, 0, value)
+    hotkey_convertions[value] = name
+    id = wx.NewIdRef().GetId()
+    res = frame.RegisterHotKey(id, 0, value)
     if res:
+        hotkey_ids[name] = id
         index = hotkeys.Append((name,))
         hotkeys.SetItemData(index, len(hotkey_names))
         hotkey_names.append(name)
-        hotkey_ids[name] = state.id
-        hotkey_alternatives[name] = keys
-        state.id -= 1
     return res
 
 
-def register_alternative(alt):
+def register_alternative(hotkey, name, keys):
     """Add an alternative."""
+    alt = Alternative(name, keys)
+    hotkey_alternatives.setdefault(hotkey, []).append(alt)
+    return alt
+
+
+def add_alternative(alt):
+    """Add an alternative to the alternatives list."""
     alternatives.Append((alt.name, alt.keys))
 
 
@@ -147,38 +189,39 @@ def on_hotkey_selected(event):
     index = hotkeys.GetFocusedItem()
     if index != -1:
         name = hotkey_names[index]
-        for alternative in hotkey_alternatives[name]:
-            register_alternative(alternative)
+        for alternative in hotkey_alternatives.get(name, []):
+            add_alternative(alternative)
 
 
-@bind(register, wx.EVT_BUTTON)
+@bind(register_hotkey_button, wx.EVT_BUTTON)
 def on_register(event):
     """Register a new hotkey."""
     name = get_key()
     if name is not None:
-        if not register_hotkey(name, []):
+        if not register_hotkey(name):
             message('Failed to register hotkey.', 'Error')
 
 
-@bind(unregister, wx.EVT_BUTTON)
+@bind(unregister_hotkey_button, wx.EVT_BUTTON)
 def on_unregister(event):
     """Unregister a hotkey."""
     index = hotkeys.GetFocusedItem()
     if index == -1:
         return wx.Bell()
-    name = hotkey_names.pop(index)
-    id = hotkey_ids.pop(name)
+    name = hotkey_names[index]
+    del hotkey_names[index]
     hotkeys.DeleteItem(index)
     alternatives.DeleteAllItems()
-    del hotkey_alternatives[name]
-    frame.Unbind(wx.EVT_HOTKEY, id=id)
+    if name in hotkey_alternatives:
+        del hotkey_alternatives[name]
+    id = hotkey_ids.pop(name)
     if frame.UnregisterHotKey(id):
         message('Hotkey unregistered.', 'Success')
     else:
         message('Failed to unregister hotkey.', 'Error')
 
 
-@bind(add_alternative, wx.EVT_BUTTON)
+@bind(add_alternative_button, wx.EVT_BUTTON)
 def on_add_alternative(event):
     """Add an alternative."""
     index = hotkeys.GetFocusedItem()
@@ -191,12 +234,12 @@ def on_add_alternative(event):
             default_value=keys
         )
         if name:
-            alt = Alternative(name, keys)
-            hotkey_alternatives[hotkey_names[index]].append(alt)
-            register_alternative(alt)
+            hotkey = hotkey_names[index]
+            alt = register_alternative(hotkey, name, keys)
+            add_alternative(alt)
 
 
-@bind(remove_alternative, wx.EVT_BUTTON)
+@bind(remove_alternative_button, wx.EVT_BUTTON)
 def on_remove_alternative(event):
     """Remove an alternative."""
     index = hotkeys.GetFocusedItem()
@@ -215,19 +258,18 @@ if __name__ == '__main__':
         with open(filename, 'r') as f:
             data = load(f, Loader=FullLoader)
         if data is not None:
-            for name, alts in data['hotkeys'].items():
-                register_hotkey(
-                    name, list(
-                        Alternative(a['name'], a['keys']) for a in alts
-                    )
-                )
+            for hotkey in data.get('hotkeys', []):
+                register_hotkey(hotkey)
+                for entry in data.get('alternatives', {}).get(hotkey, []):
+                    register_alternative(hotkey, entry['name'], entry['keys'])
     frame.Show(True)
     frame.Maximize()
     app.MainLoop()
-    data = dict(hotkeys={})
-    for name, alternatives in hotkey_alternatives.items():
-        data['hotkeys'][name] = []
-        for a in alternatives:
-            data['hotkeys'][name].append(dict(name=a.name, keys=a.keys))
+    data = dict(hotkeys=[], alternatives={})
+    for name in hotkey_names:
+        data['hotkeys'].append(name)
+        data['alternatives'][name] = []
+        for a in hotkey_alternatives.get(name, []):
+            data['alternatives'][name].append(dict(name=a.name, keys=a.keys))
     with open(filename, 'w') as f:
         dump(data, stream=f)
